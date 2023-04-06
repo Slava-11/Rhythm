2186dc25414b0e87ddcfc31a12cb39d6ff1d8c1e
Vyacheslav_Rudenko <rudenkoslava787@gmail.com>
2023-04-06 20:01:20 +0300

    2.0

--- src/js/anchor-link.js
+++ src/js/anchor-link.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
 const anchors= document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
@@ -10,3 +11,4 @@ for (let anchor of anchors) {
         })
     })
 }
+});
